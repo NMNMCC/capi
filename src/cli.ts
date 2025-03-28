8cf058f41d55dddaf41e7d9238125e14a695c51a
intzaaa <huangyinhaow@outlook.com>
2025-03-28 19:51:09 +0800

    0.0.10

--- src/cli.ts
+++ src/cli.ts
@@ -3,9 +3,11 @@
 import { app } from ".";
 import { serve } from "@hono/node-server";
 
+const host = process.env["HOST"] || "[::]";
 const port = Number(process.env["PORT"]) || 3000;
 
 serve({
     fetch: app.fetch.bind(app),
+    hostname: host,
     port: port,
 });
